49cad86a864e3fdbb3a0ea2501935d89b6d02839
Pramod0347 <pgoudar347@gmail.com>
2025-04-04 21:43:32 +0530

    git google key revoked and video page reimplemented

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,47 +1,63 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
-import { YOUTUBE_VIDEO_URL } from '../utils/constants';
+import { YOUTUBE_BASE_URL } from '../utils/constants';
+import GOOGLE_API_KEY from '../utils/constants';
 
 const WatchPage = () => {
+    const [searchParams] = useSearchParams();
+    const videoId = searchParams.get('v');
+    const [videoDetails, setVideoDetails] = useState(null);
 
     useEffect(() => {
-
-        console.log('WatchPage', YOUTUBE_VIDEO_URL);
-        
         const getVideoDetails = async () => {
+            if (!videoId) return;  // Ensure video ID exists
+            
             try {
-                const response = await fetch(YOUTUBE_VIDEO_URL);
+                const response = await fetch(
+                    `${YOUTUBE_BASE_URL}id=${videoId}&key=${GOOGLE_API_KEY}`
+                );
                 const data = await response.json();
-                console.log(data);
+
+                if (data.items && data.items.length > 0) {
+                    setVideoDetails(data.items[0]);  // Store video details in state
+                } else {
+                    console.error("No video found for this ID");
+                }
             } catch (error) {
                 console.error("Error fetching videos:", error);
             }
         };
         getVideoDetails();
-    }, []);
-
-    const [searchParams] = useSearchParams();
-
-    console.log('Params', searchParams.get('v'));
+    }, [videoId]);
 
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(closeMenu());
     }, [dispatch]);
+
     return (
         <div className='p-4'>
-            <iframe
-                width="1200" height="600" 
-                src={`https://www.youtube.com/embed/${searchParams.get('v')}?si=rwIB2f879i8aLyYi`}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerPolicy="strict-origin-when-cross-origin" 
-                allowFullScreen>
-            </iframe>
+            {videoDetails ? (
+                <div>
+                    <h2 className="text-xl font-bold mb-4">{videoDetails.snippet.title}</h2>
+                    <iframe
+                        width="1200"
+                        height="600"
+                        src={`https://www.youtube.com/embed/${videoId}`}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                    ></iframe>
+                    <p className="mt-2">{videoDetails.snippet.description}</p>
+                </div>
+            ) : (
+                <p>Loading video...</p>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default WatchPage;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
-const GOOGLE_API_KEY = 'AIzaSyBezDvWkBLV3WuaoLAlYOhUTDYlOxfmVCs';  
+const GOOGLE_API_KEY = '--';  
 export const YOUTUBE_API_URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ GOOGLE_API_KEY;
-export const YOUTUBE_VIDEO_URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=SE2XPcVpnhI&key='+ GOOGLE_API_KEY;
+export const YOUTUBE_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&';
+
+export default GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_URL = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=' + GOOGLE_API_KEY;
